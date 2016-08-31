582741dad8cc62556f9af00481a414e91dc40240
RollMan <qwpmb554@gmail.com>
2016-08-31 22:28:52 +0900

    Add generator and validator

--- rollman1/tests/validator.py
+++ rollman1/tests/validator.py
@@ -18,7 +18,7 @@ S_MAX = 100
 def main():
     line = input()
     assert len(line.split()) == 3, 'Too many or too less argument(s) at line 1'
-    m = re.match(r'^0|[1-9]\d* 0|[1-9]\d*|\d.\.\d. 0|[1-9]\d*$', line)
+    m = re.match(r'^(0|[1-9](\d)*) (0|[1-9](\d)*|(\d)+\.(\d)+) (0|[1-9](\d*))$', line)
     assert m, 'strange line \"{}\" at 1'.format(line.strip())
     line_split = line.split()
     L = int(line_split[0])
@@ -27,6 +27,7 @@ def main():
     assert L_MIN <= L <= L_MAX, 'L out of range: {}'.format(L)
     assert p_MIN < p <= p_MAX, 'rho out of range: {}'.format(p)
     assert d_MIN <= d <= d_MAX, 'd out of range: {}'.format(d)
+    assert d <= L, 'd <= L is false, that is, !{} <= {}'.format(d, L)
 
     lines = sys.stdin.readlines()
     assert len(lines) == 2, '{} extra line(s) detected.'.format( len(lines) )
