71237ad916d57ec00347b53e534ff03c6fb41b0a
arrows-1011 <s1210207@gmail.com>
2016-09-01 21:09:12 +0900

    Update generator

--- dohatsu7/tests/generator.cc
+++ dohatsu7/tests/generator.cc
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     registerGen(argc, argv, 1);
     
-    for (int t = 0; t < 100; t++) {
+    for (int t = 0; t < 20; t++) {
         ofstream of(format("03_random_%02d.in", t+1).c_str());
         int N, M;
         N = rnd.next(1, 5000);
