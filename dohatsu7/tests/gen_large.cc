a8304a46b75883b95117a7b40236d69fe4030f41
arrows-1011 <s1210207@gmail.com>
2016-09-03 23:44:19 +0900

    Add generator

---
+++ dohatsu7/tests/gen_large.cc
@@ -0,0 +1,26 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    registerGen(argc, argv, 1);
+    
+    for (int t = 0; t < 10; t++) {
+        ofstream of(format("02_large_%02d.in", t+1).c_str());
+        int N, M;
+        N = rnd.next(4000, 5000);
+        M = rnd.next(80000, 100000);
+        of << N << " " << M << endl;
+        for (int i = 0; i < M; i++) {
+            int a, b, c;
+            a = rnd.next(1, N-1);
+            b = rnd.next(1, N);
+            c = rnd.next(1, 100000);
+            of << a << " " << b << " " << c << endl;
+        }        
+        of.close();
+    }        
+    return 0;
+}
