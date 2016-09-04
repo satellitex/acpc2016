64950d5058502eb6c69f9b03f7611182066eff24
kawabys <s.kawabayashi@gmail.com>
2016-09-04 22:07:01 +0900

    メッセージ

---
+++ rollman2/kawabys/kawabys.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int c[51][51],m,n,o,p,x,y,z,an;
+int main(){
+  cin>>m>>n>>o>>p;
+  for(int i=0;i<p;i++)cin>>x>>y>>z,c[x][y]=1;
+  for(int i=0;i<n;i++)
+    for(int j=0;j<m;j++)cin>>x,an+=c[i][j]*x;
+  cout<<an<<endl;
+  return 0;
+}
