1883c13c55d2defb45ba869e9be2c6d4f35d8bee
kawabys <s.kawabayashi@gmail.com>
2016-09-05 17:28:43 +0900

    message

--- rollman2/kawabys/kawabys.cpp
+++ rollman2/kawabys/kawabys.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int c[51][51],m,n,o,p,x,y,z,an;
 int main(){
   cin>>m>>n>>o>>p;
-  for(int i=0;i<p;i++)cin>>x>>y>>z,c[x][y]=1;
+  for(int i=0;i<p;i++)cin>>x>>y>>z,c[x][y]++;
   for(int i=0;i<n;i++)
-    for(int j=0;j<m;j++)cin>>x,an+=c[i][j]*x;
+    for(int j=0;j<m;j++)cin>>x,an+=c[j][i]*x;
   cout<<an<<endl;
   return 0;
 }
