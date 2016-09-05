2baee4562c73dadea45b773d6b653837335ec4c9
s1230149 <s1230149@gmail.com>
2016-09-05 14:49:43 +0900

    update

--- s123_1/haji/haji.cpp
+++ s123_1/haji/haji.cpp
@@ -9,37 +9,36 @@ int dp[31][31][5001];
 int main(){
   int n,m,r;
   cin>>n>>m>>r;
-  
   for(int i=0;i<n;i++) 
     for(int j=0;j<n;j++)G[i][j]=INF;
-  
+
   for(int i=0,a,b,c;i<m;i++){
     cin>>a>>b>>c;
     G[a-1][b-1]=G[b-1][a-1]=c;
   }
   
   for(int i=0;i<n;i++) cin>>ball[i];
-  for(int i=0;i<n;i++) G[i][i]=0;
   
   for(int k=0;k<n;k++)
     for(int i=0;i<n;i++)
       for(int j=0;j<n;j++)
 	G[i][j]=min(G[i][j],G[i][k]+G[k][j]);	
+  for(int i=0;i<n;i++)G[i][i]=15;
   
   memset(dp,-1,sizeof(dp));
   dp[0][0][0]=0;
   for(int t=0;t<=r;t++) //time
-    for(int j=0;j<n;j++)//pos
-      for(int i=0;i<n;i++) //pre
+    for(int i=0;i<n;i++) //pre
+      for(int j=0;j<n;j++)//pos
 	for(int k=0;k<n;k++){//next
-	  if(dp[i][j][t]==-1||G[j][k]==INF)continue;
+	  if(dp[i][j][t]==-1)continue;
 	  int nball=dp[i][j][t]+ball[k];
-	  if(i!=k&&j!=k)dp[j][k][t+G[j][k]]=max(dp[j][k][t+G[j][k]],nball);
+	  if(i!=k)dp[j][k][t+G[j][k]]=max(dp[j][k][t+G[j][k]],nball);
 	  else {
 	    int cost=G[j][k]<8? 15-G[j][k]:G[j][k];
 	    dp[j][k][t+cost]=max(dp[j][k][t+cost],nball);
 	    int cost2=max(15,(cost+G[k][j]));
-	    if(k!=j)dp[k][j][t+cost2]=max(dp[k][j][t+cost2],nball+ball[j]);
+	    dp[k][j][t+cost2]=max(dp[k][j][t+cost2],nball+ball[j]);
 	  }
 	}
   
