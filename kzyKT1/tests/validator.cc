853d0ce5bb73be2f561bc937f9ead6d08afb102d
kzyKT <s1200008aizu@gmail.com>
2016-09-06 17:16:13 +0900

    add validator

---
+++ kzyKT1/tests/validator.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+using namespace std;
+
+int main() {
+  registerValidation();
+  int n=inf.readInt(2, 1000000000, "n");
+  inf.readSpace();
+  inf.readInt(1, n-1, "d");
+  inf.readEof();
+  return 0;
+}
