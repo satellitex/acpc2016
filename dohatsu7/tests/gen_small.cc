368366bc2043d75c29a963e1cb8f558aaef92ce8
arrows-1011 <s1210207@gmail.com>
2016-09-07 16:41:13 +0900

    Add generator_small

---
+++ dohatsu7/tests/gen_small.cc
@@ -0,0 +1,30 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    registerGen(argc, argv, 1);
+    
+    for (int t = 0; t < 50; t++) {
+        ofstream of(format("05_mysmall_%02d.in", t+1).c_str());
+        int N, M;
+        N = rnd.next(2, 5);
+        M = rnd.next(1, 15);
+        of << N << " " << M << endl;
+        for (int i = 0; i < M; i++) {
+            int a, b, c;
+            PREV:
+            a = rnd.next(1, N-1);
+            b = rnd.next(1, N);
+            if (a == b) {
+                goto PREV;
+            }                
+            c = rnd.next(1, 100000);
+            of << a << " " << b << " " << c << endl;
+        }        
+        of.close();
+    }        
+    return 0;
+}
