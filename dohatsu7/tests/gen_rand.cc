71fe4888102a952d8e686fbfd8baae2d5b4c1998
arrows-1011 <s1210207@gmail.com>
2016-09-07 15:49:16 +0900

    Update Data

--- dohatsu7/tests/gen_rand.cc
+++ dohatsu7/tests/gen_rand.cc
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     for (int t = 0; t < 10; t++) {
         ofstream of(format("03_random_%02d.in", t+1).c_str());
         int N, M;
-        N = rnd.next(1, 5000);
+        N = rnd.next(2, 5000);
         M = rnd.next(1, 100000);
         of << N << " " << M << endl;
         for (int i = 0; i < M; i++) {
