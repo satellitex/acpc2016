f542cfa40d6fb753e9ea9e0e7cb52c13a8e71f42
moti <motohiko.ave@gmail.com>
2016-09-10 20:49:44 +0900

    add L tle solution

---
+++ Teshima01/moti_tle_corner/moti_tle_corner.cc
@@ -0,0 +1,374 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i,a,b) for(int i=a;i<(int)b;i++)
+#define rep(i,n) REP(i,0,n)
+
+int const inf = 1<<29;
+
+namespace point_2d {
+using Real = double;
+
+Real const EPS = 1e-8;  // !!! DO CHECK EPS !!!
+
+typedef complex<Real> P;
+
+bool operator < (P const& l, P const& r) {
+  return abs(l.real() - r.real()) < EPS ? l.imag() < r.imag() : l.real() < r.real();
+}
+
+bool operator > (P const& l, P const& r) {
+  return abs(l.real() - r.real()) < EPS ? l.imag() > r.imag() : l.real() > r.real();
+}
+
+struct Line : public pair<P, P> {
+  Line() = default;
+  Line(P const& a, P const& b) { first = a, second = b; }  
+  const P& operator[] (int x) const { return x == 0 ? first : second; }
+  P& operator[] (int x) { return x == 0 ? first : second; }
+  Real length() const { return abs(second - first); }
+};
+
+typedef Line Segment;
+
+struct Circle {
+  P p; Real r;
+  Circle(){}
+  Circle(P const& p, Real r): r(r) { this->p = p; }
+};
+
+Real cross(P const& a, P const& b) { return imag(conj(a)*b); }
+Real dot(P const& a, P const& b) { return real(conj(a)*b); }
+
+bool intersect_lp(Line const& l, P const& p) {
+  return abs(cross(l[1]-p, l[0]-p)) < EPS;
+}
+
+bool intersect_ll(Line const& l, Line const& m) {
+  return abs(cross(l[1]-l[0], m[1]-m[0])) > EPS || // non-parallel
+         abs(cross(l[1]-l[0], m[0]-l[0])) < EPS;   // same line
+}
+
+P projection(Line const& l, P const& p) {
+  auto t = dot(p-l[0], l[0]-l[1]) / norm(l[0]-l[1]);
+  return l[0] + t*(l[0]-l[1]);
+}
+
+bool intersect_sp(Line const& s, P const& p) {
+  return abs(s[0]-p)+abs(s[1]-p)-abs(s[1]-s[0]) < EPS; // triangle inequality
+}
+
+Real distance_sp(Line const& s, P const& p) {
+  P const r = projection(s, p);
+  if(intersect_sp(s, r)) return abs(r - p);
+  return min(abs(s[0] - p), abs(s[1] - p));
+}
+
+bool intersect_cs(Circle const& c, Line const& l) {
+  return distance_sp(l, c.p) < c.r - EPS;
+}
+
+P crosspoint(const Line &l, const Line &m) {
+  auto A = cross(l[1] - l[0], m[1] - m[0]);
+  auto B = cross(l[1] - l[0], l[1] - m[0]);
+  if(abs(A) < EPS && abs(B) < EPS) return m[0]; // same line
+  if(abs(A) < EPS) assert(false && "PRECONDITION NOT SATISFIED");
+  return m[0] + B / A * (m[1] - m[0]);
+}
+
+vector<P> tangent_points(Circle const& c, P const& p) {
+  vector<P> ret;
+  auto const sec2 = norm(p - c.p);
+  auto const tan2 = max(Real(0), sec2 - c.r * c.r);
+  auto const nv = (p - c.p) * c.r * c.r / sec2;
+  auto const pv = (p - c.p) * P(0, -1) * c.r * sqrt(tan2) / sec2;
+  ret.push_back(c.p + nv + pv);
+  ret.push_back(c.p + nv - pv);
+  return ret;
+}
+
+istream& operator >> (istream& is, P& p) { Real x, y; is >> x >> y; p = P(x, y); return is; }
+}
+using namespace point_2d;
+
+namespace flow {
+
+struct edge {
+  int to; double cap, cost, rev;
+  edge(int t, double ca, double co, int r) : to(t), cap(ca), cost(co), rev(r) {}
+};
+
+struct primal_dual {
+  
+  vector<vector<edge>> G;
+  vector<double> h;
+  vector<double> dist;
+  vector<double> prevv, preve;
+  
+  primal_dual(int V) : G(V), h(V), dist(V), prevv(V), preve(V) {}
+  
+  void add_edge(int from, int to, int cap, double cost) {
+    G[from].emplace_back(to, cap, cost, G[to].size());
+    G[to].emplace_back(from, 0, -cost, G[from].size()-1);
+  }
+  
+  double min_cost_flow(int s, int t, double f) {
+    double ret = 0;
+    fill(h.begin(), h.end(), 0);
+    while(f > 0) {
+      typedef pair<double, int> P;
+      priority_queue<P, vector<P>, greater<P>> pq;
+      fill(dist.begin(), dist.end(), inf);
+      dist[s] = 0;
+      pq.emplace(0, s);
+      while(!pq.empty()) {
+        P p = pq.top(); pq.pop();
+        int v = p.second;
+        if(dist[v] < p.first) continue;
+        for(int i=0; i<G[v].size(); i++) {
+          auto& e = G[v][i];
+          if(e.cap > 0 && dist[e.to] > dist[v] + e.cost + h[v] - h[e.to]) { // TLE: 05_my_killed_case_01.in (EPSがない)
+            dist[e.to] = dist[v] + e.cost + h[v] - h[e.to];
+            prevv[e.to] = v;
+            preve[e.to] = i;
+            pq.emplace(dist[e.to], e.to);
+          }
+        }
+      }
+
+      if(dist[t] == inf) return -1;
+      for(int i=0; i<h.size(); i++) h[i] += dist[i];
+
+      double d = f;
+      for(int i=t; i!=s; i=prevv[i]) {
+        d = min(d, G[prevv[i]][preve[i]].cap);
+      }
+      f -= d;
+      ret += d * h[t];
+      for(int i=t; i!=s; i=prevv[i]) {
+        auto& e = G[prevv[i]][preve[i]];
+        e.cap -= d;
+        G[i][e.rev].cap += d;
+      }
+    }
+    return ret;
+  }
+
+};
+
+}
+
+using namespace point_2d;
+
+struct xor128 {
+  unsigned x,y,z,w;
+  xor128(): x(89165727), y(157892372), z(7777777), w(757328) {}
+  unsigned next() {
+    unsigned t=x^(x<<11);
+    x=y;y=z;z=w;
+    return w=w^(w>>19)^t^(t>>8);
+  }
+  unsigned next(unsigned k) {
+    return next()%k;
+  }
+} rndgen;
+
+namespace visualizer {
+
+////////////////////////////////////
+ofstream ofs;
+double zoom = 1;
+////////////////////////////////////
+
+string random_color() {
+  stringstream ss; ss << std::hex << rndgen.next(16777216);
+  return "#" + ss.str();
+}
+
+ostream& set_offset(P p) {
+  return ofs << "set_offset(" << p.real() << "," << p.imag() << ")" << endl;
+}
+
+ostream& line(Line l, string color = "") {
+  l[0] *= zoom, l[1] *= zoom;
+  return ofs << "line(" << l[0].real() << "," << l[0].imag() << ","
+    << l[1].real() << "," << l[1].imag() << ","
+    << (color.empty() ? "\"" + random_color() + "\"" : "\"" + color + "\"") << ")"
+    << endl;
+}
+
+ostream& circle(Circle c, string color = "") {
+  c = Circle(P(c.p.real() * zoom, c.p.imag() * zoom), c.r * zoom);
+  return ofs << "circle(" << c.p.real() << "," << c.p.imag() << "," << c.r << ","
+  << (color.empty() ? "\"" + random_color() + "\"" : "\"" + color + "\"") << ")" << endl;
+}
+
+ostream& plot(P p, string color = "") {
+  auto c = Circle(P(p.real() * zoom, p.imag() * zoom), 0.1 * zoom);
+  return ofs << "circle(" << c.p.real() << "," << c.p.imag() << "," << c.r << ","
+  << (color.empty() ? "\"" + random_color() + "\"" : "\"" + color + "\"") << ", true)" << endl;
+}
+
+void show_picture() {
+  ofs.close();
+  int r = system("open visualizer.html");
+  assert(!WEXITSTATUS(r));
+}
+
+////////////////////////////////////////////////////
+
+void init() {
+  ofs = ofstream("data.js");
+}
+
+void set_grid() {
+  int Min = -100, Max = +100;
+  for(int i=Min; i<Max; i++) {
+    line(Line(P(i, -1000), P(i, 1000)), "#eeeeee");
+    line(Line(P(-1000, i), P(1000, i)), "#eeeeee");
+  }
+}
+
+}
+
+int main() {
+
+  #ifdef __DEBUG
+  using namespace visualizer;
+  init();
+  zoom = 15;//stoi(string(argv[1]));
+  set_offset(P(300, 160));
+  set_grid();
+  #endif
+
+  int N; cin >> N;
+
+  Circle cs[2];
+  rep(i, 2) cin >> cs[i].p >> cs[i].r;
+
+  P rs[100]; rep(i, N) cin >> rs[i];
+  P bs[100]; rep(i, N) cin >> bs[i];
+
+  flow::primal_dual pd(2 * N + 2);
+  int const SRC = 2 * N;
+  int const SINK = 2 * N + 1;
+
+  vector<vector<double>> dists(N, vector<double>(N, inf));
+
+  rep(i, N) rep(j, N) {
+    auto seg = Segment(rs[i], bs[j]);
+    bool ok = 1;
+    rep(k, 2) {
+      ok &= !intersect_cs(cs[k], seg);
+    }
+
+    if(ok) {
+      dists[i][j] = min(dists[i][j], seg.length());
+      #ifdef __DEBUG
+      line(seg, "#dddddd");
+      #endif
+    }
+  }
+
+  vector<vector<Segment>> min_rsegment(N, vector<Segment>(N, Segment(P(0,0), P(1,1))));
+  vector<vector<Segment>> min_bsegment(N, vector<Segment>(N, Segment(P(0,0), P(1,1))));
+
+  rep(i, N) rep(j, N) rep(k, 2) rep(l, 2) {
+
+    auto& rpoint      = rs[i];
+    auto& bpoint      = bs[j];
+    auto& rtarcircle  = cs[k];
+    auto& btarcircle  = cs[l];
+
+    auto rtangents = tangent_points(rtarcircle, rpoint);
+    auto btangents = tangent_points(btarcircle, bpoint);
+
+    for(auto rtangent: rtangents) {
+      for(auto btangent: btangents) {
+
+        auto rtangent_line = Line(rpoint, rtangent);
+        auto btangent_line = Line(bpoint, btangent);
+
+        // TODO: rtangent_line same as btangent_line の分岐はしてないがOKか？
+
+        if(rpoint == rtangent) {
+          rtangent_line = Line(rtangent, rtangent + (rtarcircle.p - rtangent) * P(0, 1));
+        }
+
+        if(bpoint == btangent) {
+          btangent_line = Line(btangent, btangent + (btarcircle.p - btangent) * P(0, 1));
+        }
+
+        if(!intersect_ll(rtangent_line, btangent_line))
+          continue;
+
+        auto purple_point = crosspoint(rtangent_line, btangent_line);
+        auto rsegment = Segment(rpoint, purple_point);
+        auto bsegment = Segment(bpoint, purple_point);
+
+        bool intersect = 0;
+        rep(x, 2) {
+          intersect |= intersect_cs(cs[x], rsegment);
+          intersect |= intersect_cs(cs[x], bsegment);
+        }
+
+        if(intersect)
+          continue;
+
+        #ifdef __DEBUG
+        line(rsegment, "#dddddd");
+        line(bsegment, "#dddddd");
+        if(intersect_sp(rsegment, rtangent))
+          plot(rtangent, "#ff8888");
+        if(intersect_sp(bsegment, btangent))
+          plot(btangent, "#8888ff");
+        plot(purple_point, "#ff00ff");
+        #endif
+
+        //pd.add_edge(i, N + j, 1, rsegment.length() + bsegment.length());
+        auto length = rsegment.length() + bsegment.length();
+        if(dists[i][j] > length) {
+          dists[i][j] = min(dists[i][j], length);
+          min_rsegment[i][j] = rsegment;
+          min_bsegment[i][j] = bsegment;
+        }
+
+      }
+    }
+  }
+
+  rep(i, N) rep(j, N) {
+    if(dists[i][j] < inf) {
+      pd.add_edge(i, N + j, 1, dists[i][j]);
+    }
+  }
+
+  rep(i, N) {
+    pd.add_edge(SRC, i, 1, 0);
+    pd.add_edge(N + i, SINK, 1, 0);
+  }
+  
+  auto r = pd.min_cost_flow(SRC, SINK, N);  
+  if(r < 0) cout << -1 << endl;
+  else      printf("%.10f\n", r);
+  
+  #ifdef __DEBUG
+
+  rep(i, N) rep(j, N) {
+    if(dists[i][j] < inf) {
+      line(min_rsegment[i][j], "#ff88ff");
+      line(min_bsegment[i][j], "#ff88ff");
+      cout << "length: " << min_rsegment[i][j].length() + min_bsegment[i][j].length() << endl;
+    }
+  }
+
+  plot(P(), "#000000");     // 原点
+  circle(cs[0], "#ff0000"); // 赤いタコ
+  circle(cs[1], "#0000ff"); // 青いタコ
+  rep(i, N) plot(rs[i], "#ff0000"), plot(bs[i], "#0000ff"); // 赤いタコの足、青いタコの足
+  show_picture();
+  #endif
+
+  return 0;
+}
