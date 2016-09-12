875887eeffb92f01d5a46b4a7021030c2c36e1ed
satellitex <s.a.t.e.3.ths@gmail.com>
2016-09-12 18:26:42 +0900

    submit s123_1 by sate

--- s123_1/sate/sate.cpp
+++ s123_1/sate/sate.cpp
@@ -1,15 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
-struct edge{
-  int to,c;
-  edge(){}
-  edge(int to,int c):to(to),c(c){}  
-};
-
 struct state{
-  int id, pr, pf, f, r, sc;
+  int id, pr, r, sc;
   bool operator<(const state& e) const {
     return sc < e.sc;
   } 
@@ -17,57 +9,57 @@ struct state{
 
 int N,M,R;
 int D[50];
-vector<edge> G[50];
-
-int H[35][35][2][2][1111];
+const int INF = (1<<29);
+int H[35][35][1111];
+int W[35][35];
 int ans = 0;
-priority_queue<state> q;
-void update(int id, int pr, int pf, int f, int r, int sc){
+queue<state> q;
+void update(int id, int pr, int r, int sc){
   if( r > R ) return;
-  if( id == N-1 ) ans = max( sc, ans );
-  if( H[id][pr][pf][f][r] < sc ){
-    H[id][pr][pf][f][r] = sc;
-    q.push( (state){ id, pr, pf, f, r, sc } );
+  if( H[id][pr][r] < sc ){
+    H[id][pr][r] = sc;
+    q.push( (state){ id, pr, r, sc } );
   }
 }
 
 int solve(){
   memset( H, -1, sizeof( H ) );
-
-  q.push( (state){ 0, 0, 0, 1, 0, D[0] } );
-  H[0][0][0][1][0] = D[0];
-  
+  q.push( (state){ 0, 0, 0, D[0] } );
+  H[0][0][0] = D[0];
   while( !q.empty() ){
-    state p = q.top(); q.pop();    
-    //    cout << p.id << " "  << p.pr << " " << p.pf << " " << p.f << " " << p.r << " " << p.sc << endl;
-    if( H[p.id][p.pr][p.pf][p.f][p.r] > p.sc ) continue;
-    for( edge e : G[p.id] ){     
-      int nc = p.sc + D[e.to];
-      int nr = p.r + e.c;
-      if( p.pf && e.to == p.pr ){
-        if( e.c * 2 < 15 ) {
-          update( e.to, p.id, 0, 1, p.r + 15 - e.c, nc );
-          nc = p.sc;
+    state p = q.front(); q.pop();
+    if( H[p.id][p.pr][p.r] > p.sc ) continue;
+    for(int i=0;i<N;i++){
+      int nsc = p.sc + D[i];
+      int nr = p.r + W[p.id][i];      
+      if( i == p.pr || i == p.id )
+        if( 15 > W[p.id][i] * 2 ){
+          update( i, i, p.r + 15 - W[p.id][i], nsc );
+          continue;
         }
-      }
-      if( nc>p.sc ){
-        update( e.to, p.id, p.f, 1, nr, nc );
-      }
-      else
-        update( e.to, p.id, p.f, 0, nr, nc );
+      update( i, p.id, nr, nsc );      
     }
-    update( p.id, p.id, p.f, 1, p.r+15, p.sc + D[p.id] );
   }
+  for(int i=0;i<N;i++)
+    for(int j=0;j<N;j++)
+      for(int k=0;k<=R;k++)
+        if( W[i][N-1] + k <= R )
+          ans = max( ans, H[i][j][k] );      
   return ans;
 }
 
 int main(){
   cin >> N >> M >> R;
   for(int i=0;i<N;i++) cin >> D[i];
+  fill( W[0], W[35], INF );  
   for(int i=0;i<M;i++){
     int a,b,c; cin >> a >> b >> c; --a; --b;
-    G[a].push_back( edge( b, c ) );
-    G[b].push_back( edge( a, c ) );
+    W[a][b] = W[b][a] = c;
   }
+  for(int i=0;i<N;i++) W[i][i] = W[i][i] = 0;
+  for(int i=0;i<N;i++)
+    for(int j=0;j<N;j++)
+      for(int k=0;k<N;k++)
+        W[j][k] = min( W[j][k], W[j][i]+W[i][k] );
   cout << solve() << endl;
 }
