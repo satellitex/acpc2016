bf7601bdf8c9a38532569e4f81c8320728c15c3f
dohatsutsu <dohatsutsu@gmail.com>
2016-09-13 15:47:19 +0900

    update

---
+++ dohatsu3/tests/make2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+
+int main(){
+  for(int i=0;i<20;i++){
+    for(int j=0;j<10000;j++){
+      cout<< (char)('a'+i) ;
+    }
+  }
+  cout<<endl;
+  return 0;
+}
