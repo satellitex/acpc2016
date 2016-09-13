7020bb1fb61606b777f7406d5f51d3dbe4c41fb9
s1230149 <s1230149@gmail.com>
2016-09-13 17:13:01 +0900

    update

--- s123_1/tests/generator.cc
+++ s123_1/tests/generator.cc
@@ -8,8 +8,8 @@ int const R=1000;
 int main(int argc, char *argv[])
 {
   registerGen(argc, argv, 1);
-  for (int t = 0; t < 50; t++) {
-    ofstream of(format("01_random_%02d.in", t+1).c_str());
+  for (int t = 0; t < 30; t++) {
+    ofstream of(format("02_random_%02d.in", t+1).c_str());
     int n, m, r;
     n = rnd.next(3, 30);
     m = rnd.next(n-1, min(300,n*(n-1)/2));
