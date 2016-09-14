342fbca799d7eaefb4cb2a5d43b0e49edf76bb1e
s1230149 <s1230149@gmail.com>
2016-09-14 14:44:40 +0900

    update

--- s123_2/tests/generator.cc
+++ s123_2/tests/generator.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char *argv[]){
   registerGen(argc,argv,1);
-  for (int t=0;t<10;t++) {
+  for (int t=0;t<40;t++) {
     ofstream of(format("03_random_%02d.in",t+1).c_str());
     int n,m,k;
     n=rnd.next(1, 15);
