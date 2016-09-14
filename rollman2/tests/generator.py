f9e8a3f5a51361fae18ea38014bab2e2c886e576
Takuma Kageyama <uku09unk@gmail.com>
2016-09-14 19:19:40 +0900

    update

--- rollman2/tests/generator.py
+++ rollman2/tests/generator.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/Users/kagetaku/.pyenv/shims/python3
+
 # -*- coding: utf-8 -*-
 
 import random
--- rollman2/tests/validator.py
+++ rollman2/tests/validator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/Users/kagetaku/.pyenv/shims/python3
 # -*- coding: utf-8 -*-
 
 import sys
