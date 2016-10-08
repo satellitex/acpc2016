69854137cdfc1fe18cc6c2cdbe539b8ad4ea6ab3
moti <motohiko.ave@gmail.com>
2016-10-08 18:20:41 +0900

    Update

---
+++ RedBlue/tests/judge_abs1e-3.cc
@@ -0,0 +1,98 @@
+#include <cstdio>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include "testlib.h"
+
+#define MAX_FILE_NAME_LENG 1024
+#define MAX_DOUBLE_LEN 1024
+
+#define EPS (1e-3)
+
+bool validate_double(std::string s) {
+
+    if(s.empty())
+        return false;
+
+    int st = 0;
+    if(s[0] == '-')
+        st++;
+
+    if(!isdigit(s[st]))
+        return false;
+
+    bool point = false;
+
+    for(int i=st; i<s.size(); i++) {
+        if(s[i] == '.') {
+            if(point)
+                return false;
+            else
+                point = true;
+        }
+        else {
+            if(!isdigit(s[i]))
+                return false;
+        }
+    }
+
+    if(s.back() == '.')
+        return false;
+
+    return true;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 4) {
+        return 1;
+    }
+
+    char sInFile[MAX_FILE_NAME_LENG];
+    char sDiffFile[MAX_FILE_NAME_LENG];
+    char sOutFile[MAX_FILE_NAME_LENG];
+
+    FILE *fpDiff = fopen(argv[4], "r");
+    if (!fpDiff) {
+        return 1;
+    }
+
+    FILE *fpOut = fopen(argv[6], "r");
+    if (!fpOut) {
+        fclose(fpDiff);
+        return 1;
+    }
+
+    char diff_cs[MAX_DOUBLE_LEN];
+    fscanf(fpDiff, "%s", diff_cs);
+    fclose(fpDiff);
+
+    char out_cs[MAX_DOUBLE_LEN];
+    fscanf(fpOut, "%s", out_cs);
+    fclose(fpOut);
+
+    if(strcmp(diff_cs, out_cs) == 0) {
+      return 0;
+    }
+
+    std::string diff_str = diff_cs;
+    std::string out_str = out_cs;
+
+    if(!validate_double(diff_str)) {
+      return 1;
+    }
+
+    if(!validate_double(out_str)) {
+      return 1;
+    }
+
+    double diff = stod(diff_str);
+    double out = stod(out_str);
+
+    if(fabs( diff - out ) <= EPS) {
+        return 0;
+    }
+    
+    return 1;
+}
