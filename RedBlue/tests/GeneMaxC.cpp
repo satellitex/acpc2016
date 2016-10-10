5cd479d18c43d4d36b40ecf587dd2b9b718f7460
dohatsutsu <dohatsutsu@gmail.com>
2016-10-10 19:57:15 +0900

    update

--- RedBlue/tests/GeneMaxC.cpp
+++ RedBlue/tests/GeneMaxC.cpp
@@ -11,27 +11,29 @@ string i2s(int i){
 
 
 std::mt19937 mt( (int)time(0) );
-std::uniform_int_distribution<> randA(1,60);
+std::uniform_int_distribution<> randA(1,200);
 
 
 
 void solve(int ID){
-  ofstream fout ( "3_randomA_"+ i2s(ID) +".in" );
+  ofstream fout ( "3_randomC_"+ i2s(ID) +".in" );
 
   int N=100;
   
   fout<<N<<endl;
   
-  fout<<-30<<' '<<30<<' '<<30<<endl;
-  fout<<30<<' '<<-30<<' '<<30<<endl;
+  fout<<5<<' '<<15<<' '<<15<<endl;
+  fout<<-5<<' '<<-15<<' '<<15<<endl;
 
   map<int, map<int,int> > mp;
   
   for(int i=0;i<N;i++){
     while(1){
-      int x=randA(mt);
-      int y=randA(mt);
-      if(i%10==0)x=-x,y=-y;
+      int x=randA(mt)-100;
+      int y=40;
+
+      if(i%20==0)y=-y;
+       
       if(mp[x][y])continue;
       mp[x][y]=true;
       fout<<x<<' '<<y<<endl;
@@ -41,9 +43,11 @@ void solve(int ID){
   
   for(int i=0;i<N;i++){
     while(1){
-      int x= -randA(mt);
-      int y= -randA(mt);
-      if(i%10==0)x=-x,y=-y;
+      int x= randA(mt)-100;
+      int y= -40;
+      
+      if(i%20==0)y=-y;
+      
       if(mp[x][y])continue;
       mp[x][y]=true;
       fout<<x<<' '<<y<<endl;
---
+++ RedBlue/tests/GeneMaxD.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string i2s(int i){
+  stringstream ss;
+  ss<<i;
+  string res;
+  ss>>res;
+  return res;
+}
+
+
+std::mt19937 mt( (int)time(0) );
+std::uniform_int_distribution<> randA(1,200);
+
+
+
+void solve(int ID){
+  ofstream fout ( "3_randomD_"+ i2s(ID) +".in" );
+
+  int N=100;
+  
+  fout<<N<<endl;
+  
+  fout<<35<<' '<<35<<' '<<49<<endl;
+  fout<<-5<<' '<<-5<<' '<<7<<endl;
+
+  map<int, map<int,int> > mp;
+
+  fout<< -1000 <<' '<< 1000 <<endl;
+  
+  for(int i=0;i<N+N-2;i++){
+    while(1){
+      int x=randA(mt)-100;
+      int y=randA(mt)-100;
+       
+      if(mp[x][y])continue;
+      if( (x-20)*(x-20) + (y-35)*(y-35) <= 49*49 )continue;
+      if( (x+7)*(x+7) + (y+5)*(y+5) <= 7*7 )continue;
+      
+      mp[x][y]=true;
+      fout<<x<<' '<<y<<endl;
+      break;
+    }
+  }
+  
+  fout<< 1000 <<' '<< -1000 <<endl;
+  
+  fout.close();
+}
+
+int main(){
+  for(int i=1;i<=3;i++){
+    solve(i);
+  }
+  return 0;
+}
